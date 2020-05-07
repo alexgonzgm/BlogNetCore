6ad9c5eb3fdc536991673510bb376250a84f7b72
Alex <alexgonzgm@gmail.com>
2020-05-07 17:15:00 +0200

    Se crea  IRepossitory.cs y Reposistory.cs
    en el primero se definiran los métodos comúnes y sus posibles parámetros y en el segundo se implementara el interfaz y se definirá el cuerpo del método

---
+++ BlogCore.AccesoDatos/Data/Repository.cs
@@ -0,0 +1,69 @@
+﻿using BlogCore.AccesoDatos.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BlogCore.AccesoDatos.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+        internal DbSet<T> dbSet;
+        public Repository(DbContext context)
+        {
+            this.Context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            //include Properties 
+            if (includeProperties != null)
+            {
+                foreach (var includePropertie in includeProperties.Split(new char[] { ','},StringSplitOptions.RemoveEmptyEntries) )
+                {
+                    query = query.Include(includePropertie);
+                }
+            }
+            if (orderBy!= null)
+            {
+                return orderBy(query).ToList();
+            }
+
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
