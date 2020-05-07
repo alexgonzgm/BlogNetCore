92016830d5d7c1f3738b3f539c5c135cfdc1fad2
Alex <alexgonzgm@gmail.com>
2020-05-07 19:34:35 +0200

    Creacion de la base de datos en Sql Server

--- BlogCore.AccesoDatos/Migrations/20200507172347_CreacionTablaCategoria.Designer.cs
+++ BlogCore.AccesoDatos/Migrations/20200507172347_CreacionTablaCategoria.Designer.cs
@@ -4,14 +4,16 @@ using BlogCore.AccesoDatos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogCore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200507172347_CreacionTablaCategoria")]
+    partial class CreacionTablaCategoria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BlogCore.AccesoDatos/Migrations/20200507172347_CreacionTablaCategoria.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogCore.Data.Migrations
+{
+    public partial class CreacionTablaCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: false),
+                    Orden = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
